14bb2679221fc33b5672743f183ef47ab8ec713d
Jack Heseltine <heseltine@wolfram.com>
2024-01-13 10:54:26 +0100

    Implement CurrencyList

---
+++ CurrencyConverter.Blazor/Pages/CurrencyList.razor.cs
@@ -0,0 +1,24 @@
+﻿using CurrencyConverter.Blazor.Services.Generated;
+using Microsoft.AspNetCore.Components;
+
+namespace CurrencyConverter.Blazor.Pages
+{
+    public partial class CurrencyList
+    {
+        [Inject]
+        public IConverterService ConverterService { get; set; } = null!;
+
+        private IEnumerable<CurrencyData>? currencies;
+
+        private async Task RefreshAsync()
+        {
+            var response = await ConverterService.GetAllAsync();
+            currencies = response.Result;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshAsync();
+        }
+    }
+}
--- CurrencyConverter.Blazor/Program.cs
+++ CurrencyConverter.Blazor/Program.cs
@@ -1,4 +1,5 @@
 using CurrencyConverter.Blazor;
+using CurrencyConverter.Blazor.Services.Generated;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// best practice:
+builder.Services.AddHttpClient<IConverterService, ConverterService>(
+    client => client.BaseAddress = new Uri("https://localhost:5001")
+    );
 
 await builder.Build().RunAsync();
